60a6986fea12cf64fedc85d5c8c641eb85604ac8
Nicolas Kuhn <nicolas.kuhn@cnes.fr>
2021-02-18 15:04:56 +0100

    First commit

---
+++ 00-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Checking the ansible version that is used 
+echo "################################################"
+echo "Check ansible version - the results should be"
+echo "ansible 2.7.6"
+echo "################################################"
+ansible --version
+
+# Cloning openbach 
+echo "################################################"
+echo "Cloning the openbach and openbach-extra repositories"
+cd ../
+git clone https://forge.net4sat.org/openbach/openbach.git
+git clone https://forge.net4sat.org/openbach/openbach-extra.git
+echo "################################################"
+echo ""
+
+# Update the repository
+echo "################################################"
+cd openbach/ 
+git checkout openbach_20_04
+git pull
+cd ../
+cd openbach-extra/
+git checkout openbach_20_04
+git pull
+cd ../
+echo "################################################"
+echo ""
+
+# Preparing the ansible inventory
+echo "################################################"
+cd openbach-example-4-agent/
+cp nuc-inventory ../openbach/ansible/inventory/
+cd ../openbach/ansible/
+
+echo "################################################"
+echo "Testing that ansible can ping everyone"
+ansible -i inventory/nuc-inventory -u star -k -m ping all
+echo "################################################"
+echo ""
+
+echo "################################################"
+echo "Starting installation"
+ansible-playbook -i inventory/nuc-inventory uninstall.yml -u star -k -K
+ansible-playbook -i inventory/nuc-inventory install.yml -u star -k -K
+echo "################################################"
+echo ""
+
---
+++ README.md
@@ -0,0 +1,39 @@
+# OpenBACH Example 4 Agent : install, set up and run tests using OpenBACH
+
+This project provides the scripts to install, to set up and run tests using OpenBACH.
+
+This project does not prevent the reader from looking at [OpenBACH installation manual](https://wiki.net4sat.org/doku.php?id=openbach:manuals:2.x:installation_manual:index).
+(in particular for the requirements on each target and instal machines)
+
+## Usage
+
+By cloning the current repository, the user can:
+
+1. run the `00-install.sh` script to install OpenBACH
+1. run the `10-setup-project.sh` script to set up an OpenBACH project
+
+Based on this, the user can either use the auditorium scripts, the IHM or the provided scripts `20-run-executors.sh` and `30-run-quic.sh` to build and run scenarios.
+
+## Installation details
+
+If you encounter python-version related errors, we recommend you to either: 
+
+* On the installation machine
+  * Use virtual env
+  * Update your paths on the installation machine, e.g. 
+    * `vim ~/.bashrc`
+      * add `export PYTHONPATH="$PYTHONPATH:/home/kuhnn/Desktop/openbach-extra/apis"`
+      * add `export PATH="$PATH:/home/kuhnn/.local/bin"`
+    * source ~/.bashrc 
+* On the target machines
+  * Update the PYTHONPATH to use the correct version of python, e.g.
+    * `vim ~/.bashrc`
+      * add `export PYTHONPATH="$PYTHONPATH:/usr/bin/python3"`
+    * source ~/.bashrc 
+
+
+Five NUCs are exploited and the network architecture is showed below.
+Each NUC exploits `Ubuntu 20.04` with the username `star` and the root password `azerty123`.
+
+![Network architecture](nuc-archi.png)
+
