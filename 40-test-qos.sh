e395f37396211d9d458084b27ecd6fe0af23ede9
Nicolas Kuhn <nicolas.kuhn@cnes.fr>
2021-04-26 10:09:36 +0200

    Update 40-test-qos.sh

--- 40-test-qos.sh
+++ 40-test-qos.sh
@@ -2,4 +2,4 @@
 
 cd ../openbach-extra/executors/references/
 
-python3 executor_network_rate.py --controller 10.10.0.5 --login openbach --password openbach --server-entity 10.10.0.1 --client-entity 10.10.0.3 --server-ip 10.10.0.1 fournuc  --rate-limit 700
+python3 executor_network_rate.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc1 --client-entity nuc3 --server-ip 10.10.1.1 --rate-limit 700
