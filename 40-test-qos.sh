297fceac03c6c4cd02c60fc409a2342d2d8e5051
Nicolas Kuhn <nicolas.kuhn@cnes.fr>
2021-04-27 11:04:35 +0200

    10 and  40

--- 40-test-qos.sh
+++ 40-test-qos.sh
@@ -2,4 +2,5 @@
 
 cd ../openbach-extra/executors/references/
 
-python3 executor_network_rate.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc1 --client-entity nuc3 --server-ip 10.10.1.1 --rate-limit 700
+python3 executor_network_rate.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc1 --client-entity nuc2 --server-ip 10.10.1.1 --rate-limit 700 --post-processing-entity ctrl run --file time_series figure --data /home/star/results_QoS
+#--rate limit is by default in kb/s, and iperf3 -u gives a data rate around 600 kb/s
