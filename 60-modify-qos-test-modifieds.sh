cde243c9efdd8b8586552a3033b27d04dd4dafbb
Nicolas Kuhn <nicolas.kuhn@cnes.fr>
2021-04-30 16:47:27 +0200

    correction

--- 60-modify-qos-test-modifieds.sh
+++ 60-modify-qos-test-modifieds.sh
@@ -39,21 +39,21 @@ echo "test settings"
 echo "------------------------------"
 
 
-python3 executor_network_global.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc1 --client-entity nuc3 --server-ip 10.10.1.1 --client-ip 10.10.2.3 --rate-limit 1G --loss-measurement --post-processing-entity ctrl run --file time_series figure --data /home/star/results_global_no_tun/1-3
+python3 executor_network_global.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc1 --client-entity nuc3 --server-ip 10.10.1.1 --client-ip 10.10.2.3 --rate-limit 1G --loss-measurement --post-processing-entity ctrl run --file time_series figure --data /home/star/results_global_modified_QoS_no_tun/1-3
 #--rate limit is by default in kb/s, and iperf3 -u gives a data rate around 600 kb/s
 
-python3 executor_network_global.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc2 --client-entity nuc3 --server-ip 10.10.2.2 --client-ip 10.10.2.3 --rate-limit 1G --loss-measurement  --post-processing-entity ctrl run --file time_series figure --data /home/star/results_global_no_tun/2-3
+python3 executor_network_global.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc2 --client-entity nuc3 --server-ip 10.10.2.2 --client-ip 10.10.2.3 --rate-limit 1G --loss-measurement  --post-processing-entity ctrl run --file time_series figure --data /home/star/results_global_modified_QoS_no_tun/2-3
 
 
-python3 executor_network_global.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc1 --client-entity nuc2 --server-ip 10.10.1.1 --client-ip 10.10.1.2 --rate-limit 1G --loss-measurement  --post-processing-entity ctrl run --file time_series figure --data /home/star/results_global_no_tun/1-2
+python3 executor_network_global.py fournuc --controller 10.10.0.5 --login openbach --password openbach --server-entity nuc1 --client-entity nuc2 --server-ip 10.10.1.1 --client-ip 10.10.1.2 --rate-limit 1G --loss-measurement  --post-processing-entity ctrl run --file time_series figure --data /home/star/results_global_modified_QoS_no_tun/1-2
 
 echo "------------------------------"
 echo "clear interfaces"
 echo "------------------------------"
 
-python3 executor_network_configure_link.py --controller 10.10.0.5 --login openbach --password openbach testobachnuc --entity nuc1 --ifaces enp0s25 --mode egress --operation clear --bandwidth 10M --delay 10 run
+python3 executor_network_configure_link.py --controller 10.10.0.5 --login openbach --password openbach testobachnuc --entity nuc1 --ifaces ensI1 --mode egress --operation clear --bandwidth 10M --delay 10 run
 
-python3 executor_network_configure_link.py --controller 10.10.0.5 --login openbach --password openbach testobachnuc --entity nuc2 --ifaces enp0s25 --mode egress --operation clear --bandwidth 20M --delay 20 run
+python3 executor_network_configure_link.py --controller 10.10.0.5 --login openbach --password openbach testobachnuc --entity nuc2 --ifaces ensI1,ensI2 --mode egress --operation clear --bandwidth 20M --delay 20 run
 
 python3 executor_network_configure_link.py --controller 10.10.0.5 --login openbach --password openbach fournuc --entity nuc3 --ifaces ensI2 --mode egress --operation clear --bandwidth 20M --delay 20 run 
 
