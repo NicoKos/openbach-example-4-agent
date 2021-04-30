f27fa99ab2f2e317d08528d9d171edd4f866ab98
Nicolas Kuhn <nicolas.kuhn@cnes.fr>
2021-04-30 14:00:30 +0200

    40 and 50

--- 20-run-quic.sh
+++ 20-run-quic.sh
@@ -4,5 +4,5 @@ mkdir /home/star/Desktop/results_quic
 
 cd ../openbach-extra/executors/examples/
 
-python3 quic_configure_link.py --controller 10.10.0.5 --login openbach --password openbach fournuc --entity nuc2 --bandwidth-server-to-client 10M --bandwidth-client-to-server 1M --delay-server-to-client 25 --delay-client-to-server 25 --up-iface ensI1 --down-iface ensI2 --server nuc1 --server-ip 10.10.1.1 --server-implementation picoquic --client nuc3 --client-implementation picoquic --resources 10M_file.txt --download-dir /tmp/ --server-log-dir /tmp/ --client-log-dir /tmp/ --nb-runs 1 --pcaps-dir /tmp/ --report-dir /tmp/ --post-processing-entity ctrl run --file time_series figure --data /home/star/Desktop/results_quic
+python3 quic_configure_link.py --controller 10.10.0.5 --login openbach --password openbach fournuc --entity nuc3 --bandwidth-server-to-client 10M --bandwidth-client-to-server 1M --delay-server-to-client 25 --delay-client-to-server 25 --up-iface ensI1 --down-iface ensI2 --server nuc1 --server-ip 10.10.1.1 --server-implementation picoquic --client nuc3 --client-implementation picoquic --resources 10M_file.txt --download-dir /tmp/ --server-log-dir /tmp/ --client-log-dir /tmp/ --nb-runs 1 --pcaps-dir /tmp/ --report-dir /tmp/ --post-processing-entity ctrl run --file time_series figure --data /home/star/results_QoS/QUIC1-3
 
